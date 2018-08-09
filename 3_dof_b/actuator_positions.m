2b305fe82554e9f9758adc5807e03bd14e599f9c
Drew Warren <drewbwarren@gmail.com>
2018-08-09 16:45:19 -0500

    Improvements to 3_dof_b kinematics
    The kinematic equations are still not quite right.

--- 3_dof_b/actuator_positions.m
+++ 3_dof_b/actuator_positions.m
@@ -1,22 +1,23 @@
-function [Pfront,Pleft,Pright] = actuator_positions(u)
+function [P1,P2,P3] = actuator_positions(u)
 
 r = u.r;
 R = u.R;
 L1 = u.L1;
 L2 = u.L2;
+P = u.P;
 
-theta = u.theta;
-phi = u.phi;
-H = u.H;
+theta_list = u.theta;
+phi_list = u.phi;
+H_list = u.H;
 
 
 
-Pfront = sqrt(r.^2 - 2.*r.*L1.*cos(theta) + L1.^2.*cos(theta).^2 + L1.^2.*sin(theta).^2.*cos(phi).^2 + ...
-    2.*H.*L1.*sin(theta).*cos(phi) + H.^2);
-Pleft = sqrt(R.^2 + 2.*R.*L1.*sin(theta).*sin(phi) - 2.*R.*L2.*cos(phi) + L1.^2.*sin(theta).^2.*sin(phi).^2 - ...
-    2.*L1.*L2.*sin(theta).*sin(phi).*cos(phi) + L2.^2 + H.^2 - 2.*H.*L2.*sin(phi));
-Pright = sqrt(R.^2 - 2.*R.*L1.*sin(theta).*sin(phi) - 2.*R.*L2.*cos(phi) + L1.^2.*sin(theta).^2.*sin(phi).^2 + ...
-    2.*L1.*L2.*sin(theta).*sin(phi).*cos(phi) + L2.^2 + H.^2 + 2.*H.*L2.*sin(phi));
+% Pfront = sqrt(r.^2 - 2.*r.*L1.*cos(theta) + L1.^2.*cos(theta).^2 + L1.^2.*sin(theta).^2.*cos(phi).^2 + ...
+%     2.*H.*L1.*sin(theta).*cos(phi) + H.^2);
+% Pleft = sqrt(R.^2 + 2.*R.*L1.*sin(theta).*sin(phi) - 2.*R.*L2.*cos(phi) + L1.^2.*sin(theta).^2.*sin(phi).^2 - ...
+%     2.*L1.*L2.*sin(theta).*sin(phi).*cos(phi) + L2.^2 + H.^2 - 2.*H.*L2.*sin(phi));
+% Pright = sqrt(R.^2 - 2.*R.*L1.*sin(theta).*sin(phi) - 2.*R.*L2.*cos(phi) + L1.^2.*sin(theta).^2.*sin(phi).^2 + ...
+%     2.*L1.*L2.*sin(theta).*sin(phi).*cos(phi) + L2.^2 + H.^2 + 2.*H.*L2.*sin(phi));
 
 % Kinematic Equations but remove a and b
 % Pfront = sqrt(r.^2 - 2.*r.*L1.*cos(theta) + L1.^2.*cos(theta).^2 + L1.^2.*sin(theta).^2 + ...
@@ -27,5 +28,45 @@ Pright = sqrt(R.^2 - 2.*R.*L1.*sin(theta).*sin(phi) - 2.*R.*L2.*cos(phi) + L1.^2
 %      + L2.^2 + H.^2 + 2.*H.*L2.*sin(phi));
 
 
+% front, left, right
+P1 = zeros(1,length(theta_list));
+P2 = zeros(1,length(theta_list));
+P3 = zeros(1,length(theta_list));
+
+for i = 1:length(theta_list)
+    
+    theta = theta_list(i);
+    phi = phi_list(i);
+    H = H_list(i);
+    
+    Rot = [cos(theta) 0 sin(theta);
+        sin(theta)*sin(phi) cos(phi) -sin(phi)*cos(theta);
+        -sin(theta)*cos(phi) sin(phi) cos(theta)*cos(phi)];
+    Rz = [-1 0 0;
+        0 -1 0;
+        0 0 1];
+    Rot = Rot*Rz;
+    
+    
+    c = [0 0 H]';
+    
+    
+    % a - from output frame to top of linear actuator
+    a1 = [-L1 0 0]';
+    a2 = [0 -L2 0]';
+    a3 = [0 L2 0]';
+
+    % b - from base frame to bottom actuator joint
+    b1 = [r 0 .05]';
+    b2 = [0 R .05]';
+    b3 = [0 -R .05]';
+
+    p1 = c + Rot*a1 - b1;
+    p2 = c + Rot*a2 - b2;
+    p3 = c + Rot*a3 - b3;
+
+    P1(i) = norm(p1) - P;
+    P2(i) = norm(p2) - P;
+    P3(i) = norm(p3) - P;
 end
 
--- 3_dof_b/trajectory.m
+++ 3_dof_b/trajectory.m
@@ -8,20 +8,27 @@ r = .4;
 R = r*sind(60);
 L1 = .2;
 L2 = .2;
-h = .1;
+h = .2;
+
+actuator_length = 0.2;
+P = actuator_length;
 
 % Desired motion
 m = 10000;
 tf = 10;
 t = linspace(0,tf,m);
 theta = 0*sin(115/15*t)*pi/180; 
-phi = 20*sin(10*t)*pi/180;
-z = .05*sin(.5*t);
+phi = 0*sin(10*t)*pi/180;
+z = .2*sin(.5*t);
 H = h+z;
 
 
-vars = table(r,R,L1,L2,theta,phi,H);
+vars = table(r,R,L1,L2,theta,phi,H,P);
 [Pfront,Pleft,Pright] = actuator_positions(vars);
+
+% Pfront = 0*t;
+% Pleft = 0*t;
+% Pright = 0*t;
 
 Pfront = cat(2,t',Pfront');
 Pleft = cat(2,t',Pleft');
@@ -29,15 +36,15 @@ Pright = cat(2,t',Pright');
 
 % Changes to the actuator commands because of the way the prismatic joints
 % are set up
-actuator_length = 0.2;
-Pfront(:,2) = Pfront(:,2) - actuator_length*2;
-Pleft(:,2) = Pleft(:,2) - actuator_length*2;
-Pright(:,2) = Pright(:,2) - actuator_length*2;
 
-min_length = min(cat(1,Pfront(:,2),cat(1, Pleft(:,2), Pright(:,2))));
-Pfront(:,2) = Pfront(:,2) + abs(min_length);
-Pleft(:,2) = Pleft(:,2) + abs(min_length);
-Pright(:,2) = Pright(:,2) + abs(min_length);
+% Pfront(:,2) = Pfront(:,2) - actuator_length*2;
+% Pleft(:,2) = Pleft(:,2) - actuator_length*2;
+% Pright(:,2) = Pright(:,2) - actuator_length*2;
+% 
+% min_length = min(cat(1,Pfront(:,2),cat(1, Pleft(:,2), Pright(:,2))));
+% Pfront(:,2) = Pfront(:,2) + abs(min_length);
+% Pleft(:,2) = Pleft(:,2) + abs(min_length);
+% Pright(:,2) = Pright(:,2) + abs(min_length);
 
 
 run('run_device_sim.m')
